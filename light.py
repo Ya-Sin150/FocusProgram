b4a17e1ad09a74419d9c64e8235985ee9f50caf9
Yasin.S <Ya-Sin150@users.noreply.github.com>
2021-02-15 17:18:24 +0000

    Light update
    I have added some functions so the the light could be changed if switched

--- light.py
+++ light.py
@@ -1,1 +1,14 @@
-print('Hello World')
+import numpy as np
+import matplotlib
+import pandas as pd
+
+
+class Light():
+    def __init__ (self, shape, colour):
+        self.shape = shape
+        self.colour = colour
+
+    def switch():
+        #Build the switch in such a way that you can change the colour
+        #and change the shape of the light and bulb respectively
+        pass()
